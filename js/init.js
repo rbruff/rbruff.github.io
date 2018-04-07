fc5d00b66ddd105bb156cbf51b4c4fe47650fa5c
renee <reneebruff@gmail.com>
2018-04-07 15:10:18 -0400

    pushing up brief case files

---
+++ js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+});
+      
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
