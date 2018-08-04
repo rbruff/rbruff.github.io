29e7c496f7bc893229d5d22f1b70a32bdebd4bc3
renee <reneebruff@gmail.com>
2018-08-03 23:44:45 -0400

    about me update and modal initialized

--- js/init.js
+++ js/init.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $(document).ready(function(){
+      $('.modal').modal();
+    });
+  
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
